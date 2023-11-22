4ceb86c695d32a534f01860c9a831b5b96a76053
Cauã Sebastian <cauasebastian2207@gmail.com>
2023-11-22 01:23:54 -0300

    possiveis movimentos da peça

--- Sistema-jogo-de-xadrez/src/boardgame/Piece.java
+++ Sistema-jogo-de-xadrez/src/boardgame/Piece.java
@@ -1,8 +1,9 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
+
     protected Position position;
-    private  Board board;
+    private Board board;
 
     public Piece(Board board) {
         this.board = board;
@@ -11,5 +12,23 @@ public class Piece {
 
     protected Board getBoard() {
         return board;
+    }
+
+    public abstract boolean[][] possibleMoves();
+
+    public boolean possibleMove(Position position) {
+        return possibleMoves()[position.getRow()][position.getColum()];
+    }
+
+    public boolean isThereAnyPossibleMove() {
+        boolean[][] mat = possibleMoves();
+        for (int i=0; i<mat.length; i++) {
+            for (int j=0; j<mat.length; j++) {
+                if (mat[i][j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
