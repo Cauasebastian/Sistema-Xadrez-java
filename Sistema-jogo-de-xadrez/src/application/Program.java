04e4543f677b251bd4f3ed7621d5622968cdcaf9
Cauã Sebastian <cauasebastian2207@gmail.com>
2023-11-22 23:03:21 -0300

    mudando o turno dos jogadores
    fiz metodos para trocar o turno dos jogadores

--- Sistema-jogo-de-xadrez/src/application/Program.java
+++ Sistema-jogo-de-xadrez/src/application/Program.java
@@ -1,15 +1,16 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
 public class Program {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
@@ -17,7 +18,7 @@ public class Program {
         while (true) {
             try {
                 UI.clearScreen();
-                UI.printBoard(chessMatch.getPieces());
+                UI.printMatch(chessMatch);
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
@@ -25,7 +26,6 @@ public class Program {
                 boolean[][] possibleMoves = chessMatch.possibleMoves(source);
                 UI.clearScreen();
                 UI.printBoard(chessMatch.getPieces(), possibleMoves);
-
                 System.out.println();
                 System.out.print("Target: ");
                 ChessPosition target = UI.readChessPosition(sc);
